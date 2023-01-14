ac87760cf27c61001c5bc66321769eea53cba5ce
Vishnu Lokesh Reddy <vishnulokesh520@gmail.com>
2023-01-14 20:33:03 +0530

    practice

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -391,50 +391,123 @@
 // Working With Strings - Part 2
 
 // Split and join
-console.log('a+very+nice+string'.split('+'));
-console.log('Jonas Schmedtmann'.split(' '));
+// console.log('a+very+nice+string'.split('+'));
+// console.log('Jonas Schmedtmann'.split(' '));
 
-const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
+// const [firstName, lastName] = 'Jonas Schmedtmann'.split(' ');
 
-const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
-console.log(newName);
+// const newName = ['Mr.', firstName, lastName.toUpperCase()].join(' ');
+// console.log(newName);
 
-const capitalizeName = function (name) {
-  const names = name.split(' ');
-  const namesUpper = [];
+// const capitalizeName = function (name) {
+//   const names = name.split(' ');
+//   const namesUpper = [];
 
-  for (const n of names) {
-    // namesUpper.push(n[0].toUpperCase() + n.slice(1));
-    namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
-  }
-  console.log(namesUpper.join(' '));
-};
+//   for (const n of names) {
+//     // namesUpper.push(n[0].toUpperCase() + n.slice(1));
+//     namesUpper.push(n.replace(n[0], n[0].toUpperCase()));
+//   }
+//   console.log(namesUpper.join(' '));
+// };
 
-capitalizeName('jessica ann smith davis');
-capitalizeName('jonas schmedtmann');
+// capitalizeName('jessica ann smith davis');
+// capitalizeName('jonas schmedtmann');
 
-// Padding
-const message = 'Go to gate 23!';
-console.log(message.padStart(20, '+').padEnd(30, '+'));
-console.log('Jonas'.padStart(20, '+').padEnd(30, '+'));
+// // Padding
+// const message = 'Go to gate 23!';
+// console.log(message.padStart(20, '+').padEnd(30, '+'));
+// console.log('Jonas'.padStart(20, '+').padEnd(30, '+'));
 
-const maskCreditCard = function (number) {
-  const str = number + '';
-  const last = str.slice(-4);
-  return last.padStart(str.length, '*');
-};
+// const maskCreditCard = function (number) {
+//   const str = number + '';
+//   const last = str.slice(-4);
+//   return last.padStart(str.length, '*');
+// };
 
-console.log(maskCreditCard(64637836));
-console.log(maskCreditCard(43378463864647384));
-console.log(maskCreditCard('334859493847755774747'));
+// console.log(maskCreditCard(64637836));
+// console.log(maskCreditCard(43378463864647384));
+// console.log(maskCreditCard('334859493847755774747'));
 
-// Repeat
-const message2 = 'Bad waether... All Departues Delayed... ';
-console.log(message2.repeat(5));
+// // Repeat
+// const message2 = 'Bad waether... All Departues Delayed... ';
+// console.log(message2.repeat(5));
 
-const planesInLine = function (n) {
-  console.log(`There are ${n} planes in line ${'🛩'.repeat(n)}`);
-};
-planesInLine(5);
-planesInLine(3);
-planesInLine(12);
+// const planesInLine = function (n) {
+//   console.log(`There are ${n} planes in line ${'🛩'.repeat(n)}`);
+// };
+// planesInLine(5);
+// planesInLine(3);
+// planesInLine(12);
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Write a program that receives a list of variable names written in underscore_case and convert them to camelCase.
+
+The input will come from a textarea inserted into the DOM (see code below), and conversion will happen when the button is pressed.
+
+THIS TEST DATA (pasted to textarea)
+underscore_case
+ first_name
+Some_Variable 
+  calculate_AGE
+delayed_departure
+
+SHOULD PRODUCE THIS OUTPUT (5 separate console.log outputs)
+underscoreCase      ✅
+firstName           ✅✅
+someVariable        ✅✅✅
+calculateAge        ✅✅✅✅
+delayedDeparture    ✅✅✅✅✅
+
+HINT 1: Remember which character defines a new line in the textarea 😉
+HINT 2: The solution only needs to work for a variable made out of 2 words, like a_b
+HINT 3: Start without worrying about the ✅. Tackle that only after you have the variable name conversion working 😉
+HINT 4: This challenge is difficult on purpose, so start watching the solution in case you're stuck. Then pause and continue!
+
+Afterwards, test with your own test data!
+
+GOOD LUCK 😀
+*/
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
+// document.querySelector('button').addEventListener('click', function () {
+//   const inputText = document.querySelector('textarea').value.split(/\r?\n/);
+//   for (let text of inputText) {
+//     if (text.includes('_')) {
+//       let [first, second] = text.trim().split('_');
+//       let out =
+//         first.toLowerCase() +
+//         second[0].toUpperCase() +
+//         second.slice(1).toLowerCase();
+//       console.log(out);
+//       console.log(`${out.padEnd(30, ' ')}${'$'}`);
+//     } else {
+//       console.log(`${text} does not contain _`);
+//     }
+//   }
+// });
+
+// const flights =
+//   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// // 🔴 Delayed Departure from FAO to TXL (11h25)
+// //              Arrival from BRU to FAO (11h45)
+// //   🔴 Delayed Arrival from HEL to FAO (12h05)
+// //            Departure from FAO to LIS (12h30)
+// const allFlights = flights.split('+');
+// for (let eachFlight of allFlights) {
+//   let [about, from, to, time] = eachFlight.split(';');
+//   let aboutFlight = about.slice(1);
+//   let flightOut;
+//   if (aboutFlight.includes('_')) {
+//     flightOut = 'NOOO' + aboutFlight.replace('_', ' ');
+//   } else {
+//     flightOut = aboutFlight;
+//   }
+//   console.log(
+//     `${flightOut} from ${from.slice(0, 3).toUpperCase()} to ${to
+//       .slice(0, 3)
+//       .toUpperCase()} (${time.replace(':', 'h')})`
+//   );
+// }
--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,222 @@
 'use strict';
+// const bookings = [];
+
+// const createBooking = function (
+//   flightNum,
+//   numPassengers = 1,
+//   price = 199 * numPassengers
+// ) {
+//   // ES5
+//   // numPassengers = numPassengers || 1;
+//   // price = price || 199;
+
+//   const booking = {
+//     flightNum,
+//     numPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 2);
+// createBooking('LH123', 5);
+
+// createBooking('LH123', undefined, 1000);
+// console.log(bookings);
+///////////////////////////////////////
+// How Passing Arguments Works: Values vs. Reference
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Jonas Schmedtmann',
+//   passport: 24739479284,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
+
+//   if (passenger.passport === 24739479284) {
+//     alert('Checked in');
+//   } else {
+//     alert('Wrong passport!');
+//   }
+// };
+
+// // checkIn(flight, jonas);
+// // console.log(flight);
+// // console.log(jonas);
+
+// // Is the same as doing...
+// // const flightNum = flight;
+// // const passenger = jonas;
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 100000000000);
+// };
+
+// newPassport(jonas);
+// checkIn(flight, jonas);
+
+//Function returning function
+// const greetFirst = function (greetings) {
+//   return function (name) {
+//     console.log(`${greetings} ${name}`);
+//   };
+// };
+
+// //This greetFirst function returns a new function
+// const greetFirstReturnFunction = greetFirst('Good Morning');
+
+// //Call to the new function returned by greetFirst function
+// greetFirstReturnFunction('Vishnu');
+
+// //Function returning function - With Arrow functions
+// const greetSecond = greeting => names => console.log(`${greeting} ${names}`);
+// greetSecond('Hello')('V');
+
+//Create an airline object with a method in it
+//Create an another airline object with no method in it(should use the method of previous object)
+//introduce the call and apply methods and it's usecases to pass the this property
+
+// const quaterAirlines = {
+//   name: 'quatar',
+//   code: 'QUA',
+//   passengers: [],
+//   passengerData(flightNumber, pName) {
+//     console.log(
+//       `${this.name.toUpperCase()} airlines with airlines code ${
+//         this.code
+//       } and flight number: ${flightNumber} is booked for ${pName}`
+//     );
+//     this.passengers.push({
+//       flightNumber: `${this.code} and ${flightNumber}`,
+//       pName,
+//     });
+//   },
+// };
+// quaterAirlines.passengerData(7, 'Vishnu');
+// console.log(quaterAirlines.passengers);
+
+// //Now, let's assure we are opening new airlines named airIndia
+// const airIndia = {
+//   name: 'air',
+//   code: 'AIR',
+//   passengers: [],
+// };
+// const airIndiaPassengerData = quaterAirlines.passengerData;
+
+// //This would throw the error, since we are doing simple function call, the "this" keyword is undefined in the passengerData method. To overcome this, we have to use "call" or "apply" methods to pass the object name as an arugument with other arguments in the function call.
+// // airIndiaPassengerData(8, 'V');
+
+// //Intro of call method: the call method takes the object as first and then the other aruguments to function indidually
+// airIndiaPassengerData.call(airIndia, 8, 'Vis');
+
+// //Intro to apply method: the apply method takes the object as first and then the other aruguments to function in the array form
+// const arrayPassengerData = [10, 'bha'];
+// airIndiaPassengerData.apply(airIndia, arrayPassengerData);
+
+// //Note: we can pass the array to call method also with spread operator
+// airIndiaPassengerData.call(airIndia, ...arrayPassengerData);
+// console.log(airIndia.passengers);
+
+// //Define bind method
+// //Define partial parameter with bind method
+// //write buy new plane method into any airlines object
+// //Activate buy new plane with eventListiner
+
+// //Bind method intro: In call and apply methods, we have to pass the argument(for this reference) everytime we are calling the method. In the bind method, we can bind the object to the function once and call function any time without passing object.
+// const airIndiaBind = airIndiaPassengerData.bind(airIndia);
+// airIndiaBind(50, 'Bhavya');
+
+// //Partial parameter with bind: If we want to keep the flight number same for all passengers and only take the passenger name as an argument during function call. We can do it with bind
+// const airIndiaBind70Flight = airIndiaPassengerData.bind(airIndia, 70);
+// airIndiaBind70Flight('Bhavya H'); //In this function call, we just have to pass the name argument since flight number is already set in airIndiaBind70Flight bind.
+
+// //Let's add some parameters and methods to the airIndia object
+// airIndia.noOfPlanes = 300;
+// airIndia.buyNewPlane = function () {
+//   console.log(this);
+//   this.noOfPlanes++;
+//   console.log(this.noOfPlanes);
+// };
+
+// //Let's invoke the above function when **Buy new plane** button is clicked: the below one does not work because the callback function(airIndia.buyNewPlane) takes the this keyword from document.querySelector('.buy') and thus some statements with this in function(airIndia.buyNewPlane) throws NaN. To overcome this error, we have to use bind method in the callback function call
+// document.querySelector('.buy').addEventListener('click', airIndia.buyNewPlane);
+
+// //FIx for above
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', airIndia.buyNewPlane.bind(airIndia));
+
+// /****Partial application using bind****/
+// const totalBill = function (tip, bill) {
+//   return bill + (bill * tip) / 100;
+// };
+// console.log(totalBill(10, 100));
+
+// //Let's say the tip is fixed to 20%, we just have to pass the bill value. Then we use the bind method to achive that
+// const bindTip20 = totalBill.bind(null, 20); //Here, we need not pass the first argument with this argument. Since, we are not using this in the function. We should simple pay "null" as argument
+// console.log(bindTip20(100));
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+const poll = {
+  question: 'What is your favourite programming language?',
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+  // This generates [0, 0, 0, 0]. More in the next section 😃
+  answers: new Array(4).fill(0),
+  registerNewAnswer() {
+    let inputOption = window.prompt(
+      'What is your favourite programming language?\n0: JavaScript\n1: Python\n2: Rust\n3: C++\n(Write option number)'
+    );
+    if (Number(inputOption) > 3) {
+      window.alert('wrong input type');
+    } else {
+      this.answers[Number(inputOption)]++;
+      this.displayResults('array');
+      this.displayResults('string');
+    }
+  },
+  displayResults(type = 'array') {
+    if (type == 'array') console.log(this.answers);
+    if (type == 'string')
+      console.log(
+        `The poll results are ${this.answers[0]}, ${this.answers[1]}, ${this.answers[2]}, ${this.answers[3]} `
+      );
+  },
+};
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
+poll.registerNewAnswer();
