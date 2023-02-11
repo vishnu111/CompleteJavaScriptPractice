e586c635cd10333eb479110ada95d9b1a7465051
Vishnu Lokesh Reddy <vishnulokesh520@gmail.com>
2023-02-12 03:01:18 +0530

    End of Bankist projectt

--- 14-OOP/starter/script.js
+++ 14-OOP/starter/script.js
@@ -294,37 +294,210 @@
 // Student.prototype.constructor = Student;
 // console.dir(Student.prototype.constructor);
 
-const Car = function (make, speed) {
-  this.make = make;
-  this.speed = speed;
-};
-Car.prototype.accelerate = function () {
-  this.speed = this.speed + 10;
-  console.log(this.speed);
-};
-Car.prototype.break = function () {
-  this.speed = this.speed - 10;
-  console.log(this.speed);
-};
+// const Car = function (make, speed) {
+//   this.make = make;
+//   this.speed = speed;
+// };
+// Car.prototype.accelerate = function () {
+//   this.speed = this.speed + 10;
+//   console.log(this.speed);
+// };
+// Car.prototype.break = function () {
+//   this.speed = this.speed - 10;
+//   console.log(this.speed);
+// };
 
-const EV = function (make, speed, charge) {
-  this.make = make;
-  this.speed = speed;
-  this.charge = charge;
-};
-EV.prototype = Object.create(Car.prototype);
+// const EV = function (make, speed, charge) {
+//   this.make = make;
+//   this.speed = speed;
+//   this.charge = charge;
+// };
+// EV.prototype = Object.create(Car.prototype);
 
-EV.prototype.chargeBattery = function (curCharge) {
-  this.charge = curCharge;
-};
-EV.prototype.accelerate = function () {
-  this.speed = this.speed + 20;
-  this.charge -= 1;
-  console.log(
-    `Tesla going at ${this.speed} km/h, with a charge of ${this.charge}%`
-  );
-};
-const tesl = new EV('tesla', 120, 23);
-tesl.accelerate();
-tesl.chargeBattery(100);
-tesl.accelerate();
+// EV.prototype.chargeBattery = function (curCharge) {
+//   this.charge = curCharge;
+// };
+// EV.prototype.accelerate = function () {
+//   this.speed = this.speed + 20;
+//   this.charge -= 1;
+//   console.log(
+//     `Tesla going at ${this.speed} km/h, with a charge of ${this.charge}%`
+//   );
+// };
+// const tesl = new EV('tesla', 120, 23);
+// tesl.accelerate();
+// tesl.chargeBattery(100);
+// tesl.accelerate();
+
+///////////////////////////////////////
+// Inheritance Between "Classes": ES6 Classes
+
+// class PersonCl {
+//   constructor(fullName, birthYear) {
+//     this.fullName = fullName;
+//     this.birthYear = birthYear;
+//   }
+
+//   // Instance methods
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   }
+
+//   greet() {
+//     console.log(`Hey ${this.fullName}`);
+//   }
+
+//   get age() {
+//     return 2037 - this.birthYear;
+//   }
+
+//   set fullName(name) {
+//     if (name.includes(' ')) this._fullName = name;
+//     else alert(`${name} is not a full name!`);
+//   }
+
+//   get fullName() {
+//     return this._fullName;
+//   }
+
+//   // Static method
+//   static hey() {
+//     console.log('Hey there 👋');
+//   }
+// }
+// //with extends keyword we are inheriting PersonCl properties and methods
+// class StudentCl extends PersonCl {
+//   //If we are passing only fullName and birthYear to the constructor, then we don't need this constructor here because the parent constructor accepts those two parameters
+//   constructor(fullName, birthYear, course) {
+//     // Always needs to happen first!, super function call should be first in the constructor, it calls the parent constructor with passing arguments
+//     super(fullName, birthYear);
+//     this.course = course;
+//   }
+
+//   introduce() {
+//     console.log(`My name is ${this.fullName} and I study ${this.course}`);
+//   }
+
+//   //Overriding the parent calcAge method here
+//   calcAge() {
+//     console.log(
+//       `I'm ${
+//         2037 - this.birthYear
+//       } years old, but as a student I feel more like ${
+//         2037 - this.birthYear + 10
+//       }`
+//     );
+//   }
+// }
+
+// const martha = new StudentCl('Martha Jones', 2012, 'Computer Science');
+// martha.introduce();
+// martha.calcAge();
+///////////////////////////////////////
+// Inheritance Between "Classes": Object.create
+
+// const PersonProto = {
+//   calcAge() {
+//     console.log(2037 - this.birthYear);
+//   },
+
+//   init(firstName, birthYear) {
+//     this.firstName = firstName;
+//     this.birthYear = birthYear;
+//   },
+// };
+
+// const steven = Object.create(PersonProto);
+
+// const StudentProto = Object.create(PersonProto);
+// StudentProto.init = function (firstName, birthYear, course) {
+//   PersonProto.init.call(this, firstName, birthYear);
+//   this.course = course;
+// };
+
+// StudentProto.introduce = function () {
+//   // BUG in video:
+//   // console.log(`My name is ${this.fullName} and I study ${this.course}`);
+
+//   // FIX:
+//   console.log(`My name is ${this.firstName} and I study ${this.course}`);
+// };
+
+// const jay = Object.create(StudentProto);
+// jay.init('Jay', 2010, 'Computer Science');
+// jay.introduce();
+// jay.calcAge();
+
+class Account {
+  // 1) Public fields (instances)
+  locale = navigator.language;
+
+  // 2) Private fields (instances)
+  #movements = [];
+  #pin;
+
+  constructor(owner, currency, pin) {
+    this.owner = owner;
+    this.currency = currency;
+    this.#pin = pin;
+
+    // Protected property
+    // this._movements = [];
+    // this.locale = navigator.language;
+
+    console.log(`Thanks for opening an account, ${owner}`);
+  }
+
+  // 3) Public methods
+
+  // Public interface
+  getMovements() {
+    return this.#movements;
+  }
+
+  deposit(val) {
+    this.#movements.push(val);
+    return this;
+  }
+
+  withdraw(val) {
+    this.deposit(-val);
+    return this;
+  }
+
+  requestLoan(val) {
+    // if (this.#approveLoan(val)) {
+    if (this._approveLoan(val)) {
+      this.deposit(val);
+      console.log(`Loan approved`);
+      return this;
+    }
+  }
+
+  static helper() {
+    console.log('Helper');
+  }
+
+  // 4) Private methods
+  // #approveLoan(val) {
+  _approveLoan(val) {
+    return true;
+  }
+}
+
+const acc1 = new Account('Jonas', 'EUR', 1111);
+
+// acc1._movements.push(250);
+// acc1._movements.push(-140);
+// acc1.approveLoan(1000);
+
+acc1.deposit(250);
+acc1.withdraw(140);
+acc1.requestLoan(1000);
+console.log(acc1.getMovements());
+console.log(acc1);
+Account.helper();
+
+// console.log(acc1.#movements);
+// console.log(acc1.#pin);
+// console.log(acc1.#approveLoan(100));
