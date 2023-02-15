727a21649ceb65ef5f341d9d832b1ac4d7cf2e04
Vishnu Lokesh Reddy <vishnulokesh520@gmail.com>
2023-02-16 03:53:09 +0530

    End of Bankist projectt

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,6 +10,7 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+let map, mapEvent;
 
 //Implementing navigator API to fetch current coordinates
 //this has two callback functions, if you allow the location in browser when requested it calls the first function, if you decline it calls the second function
@@ -25,7 +26,7 @@ navigator.geolocation.getCurrentPosition(
     //From leaflet library
 
     //Here, '13' is the zoom level
-    const map = L.map('map').setView(coords, 13);
+    map = L.map('map').setView(coords, 13);
 
     //the map that loads up in the page is group of tiles, those tiles comes from the below url(from openstreetmap which is a open source map everyone can use for free), like openstreetmap here, the leaflet can work with other kinds of map like google maps
     //The default style of openstreet map is "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
@@ -34,29 +35,34 @@ navigator.geolocation.getCurrentPosition(
       attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
     }).addTo(map);
-
     //adding eventlistener to map to add markers at clicked positons using leaflet object 'map'
     //Here on is a leaflet's function
-    map.on('click', function (mapEvent) {
-      const { lat, lng } = mapEvent.latlng;
-      //This marker method and inner methods of leaflet's library documentation can be found here "https://leafletjs.com/reference.html#marker"
-      L.marker([lat, lng])
-        .addTo(map)
-        .bindPopup(
-          L.popup({
-            //options passed to popup method
-            maxWidth: 250,
-            minWidth: 100,
-            autoClose: false,
-            closeOnClick: false,
-            className: 'running-popup',
-          })
-        )
-        .setPopupContent('workout')
-        .openPopup();
+    map.on('click', function (mapE) {
+      mapEvent = mapE;
+      form.classList.remove('hidden');
+      inputDistance.focus();
     });
   },
   function () {
     alert('Could not get your coords since you declined');
   }
 );
+
+form.addEventListener('submit', function () {
+  const { lat, lng } = mapEvent.latlng;
+  //This marker method and inner methods of leaflet's library documentation can be found here "https://leafletjs.com/reference.html#marker"
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        //options passed to popup method
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('workout')
+    .openPopup();
+});
