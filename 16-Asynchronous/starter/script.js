4147f63cb9f10ed283d8d32c7f448e0e81d83563
Vishnu Lokesh Reddy <vishnulokesh520@gmail.com>
2023-02-18 20:15:42 +0530

    Mapty project initial filest

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -4,3 +4,33 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+//one way of AJAX call to an
+const getCountryData = function (country) {
+  const request = new XMLHttpRequest();
+  request.open('GET', `https://restcountries.com/v3.1/name/${country}`);
+  request.send();
+  request.addEventListener('load', function () {
+    const [data] = JSON.parse(this.responseText);
+    // console.log(data);
+    const html = `
+  <article class="country">
+  <img class="country__img" src="${data.flags.svg}" />
+  <div class="country__data">
+    <h3 class="country__name">${data.name.official}</h3>
+    <h4 class="country__region">${data.region}</h4>
+    <p class="country__row"><span>👫</span>${(
+      +data.population / 1000000
+    ).toFixed(1)}M People</p>
+    <p class="country__row"><span>🗣️</span>${data.languages.eng}</p>
+    <p class="country__row"><span>💰</span>${data.currencies.name}</p>
+  </div>
+</article>
+  `;
+    countriesContainer.insertAdjacentHTML('beforeend', html);
+    countriesContainer.style.opacity = 1;
+  });
+};
+// getCountryData('india');
+getCountryData('india');
+getCountryData('usa');
+getCountryData('pakistan');
